069414ef40f8b0067540095c241dd84b395417d1
Fred Michael <fred.michael@flash-coffee.com>
2022-11-23 09:34:31 +0700

    clean project

--- src/components/Common/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {ViewProps} from 'react-native';
-
-export const Wrapper = (props: ViewProps) => {
-  const {children} = props;
-  return <view style={{flex: 1, flexDirection: 'column'}}>{children}</view>;
-};
-
-export default Wrapper;
