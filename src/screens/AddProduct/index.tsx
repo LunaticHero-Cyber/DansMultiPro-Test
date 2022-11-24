0f90b876dd139bcc4d5fa37a9b9e8da7faf3d436
Fred Michael <fred.michael@flash-coffee.com>
2022-11-24 12:53:37 +0700

    Finish add product screen

--- src/screens/AddProduct/index.tsx
+++ src/screens/AddProduct/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-
-import {StackNavigationProp} from '@react-navigation/stack';
-
-import {Header, Container, BoxSpace} from '../../components/Common';
-import {StackParamList} from '@navigators/Root';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   Text,
+  TextInput,
   TextStyle,
   TouchableOpacity,
   View,
   ViewStyle,
 } from 'react-native';
+
+import {StackNavigationProp} from '@react-navigation/stack';
+
+import {Header, Container, BoxSpace, Wrapper} from '../../components/Common';
+import {StackParamList} from '@navigators/Root';
 import {COLORS} from 'constants/colors';
+import {SIZES} from 'constants/sizes';
+import {addProduct} from 'services/post/addProduct';
 
 type OrderRequestDetailScreenProp = StackNavigationProp<
   StackParamList,
@@ -20,18 +23,51 @@ type OrderRequestDetailScreenProp = StackNavigationProp<
 >;
 
 type HomeStyleInterface = {
-  backButton: ViewStyle;
+  button: ViewStyle;
+  activeButton: ViewStyle;
+  disabledButton: ViewStyle;
+  mainContainer: ViewStyle;
+  input: ViewStyle;
+  largeInput: ViewStyle;
   backButtonText: TextStyle;
+  headlineText: TextStyle;
 };
 
 const styles = StyleSheet.create<HomeStyleInterface>({
-  backButton: {
+  button: {
     borderRadius: 8,
     padding: 12,
+  },
+  activeButton: {
     backgroundColor: COLORS.BLUE,
+  },
+  disabledButton: {
+    backgroundColor: COLORS.BLACK60,
+  },
+  mainContainer: {
+    flex: 1,
+    padding: SIZES.medium,
+  },
+  input: {
+    borderRadius: SIZES.extraSmall,
+    borderWidth: SIZES.outline,
+    backgroundColor: COLORS.BLACK10,
+    padding: SIZES.medium,
+  },
+  largeInput: {
+    borderRadius: SIZES.extraSmall,
+    borderWidth: SIZES.outline,
+    backgroundColor: COLORS.BLACK10,
+    padding: SIZES.medium,
+    height: SIZES.pinSpacing,
+    textAlignVertical: 'top',
   },
   backButtonText: {
     color: COLORS.WHITE,
+  },
+  headlineText: {
+    fontSize: SIZES.extraLarge,
+    color: COLORS.BLACK100,
   },
 });
 
@@ -40,6 +76,17 @@ const AddProduct = ({
 }: {
   navigation: OrderRequestDetailScreenProp;
 }) => {
+  const [productName, setProductName] = useState('');
+  const [categoryName, setCategoryName] = useState('');
+  const [sku, setSku] = useState('');
+  const [description, setDescription] = useState('');
+  const [weight, setWeight] = useState('');
+  const [width, setWidth] = useState('');
+  const [length, setLength] = useState('');
+  const [height, setHeight] = useState('');
+  const [image, setImage] = useState('');
+  const [price, setPrice] = useState('');
+
   const onPressAddProductButton = () => {
     navigation.goBack();
   };
@@ -47,15 +94,153 @@ const AddProduct = ({
   const BackButton = () => (
     <TouchableOpacity
       onPress={onPressAddProductButton}
-      style={styles.backButton}>
+      style={[styles.button, styles.activeButton]}>
       <Text style={styles.backButtonText}>Back</Text>
     </TouchableOpacity>
   );
 
+  const SubmitFormButton = () => (
+    <TouchableOpacity
+      onPress={onPressSubmitFormButton}
+      style={[
+        isAllFilled() ? styles.activeButton : styles.disabledButton,
+        styles.button,
+      ]}
+      disabled={!isAllFilled()}>
+      <Text style={styles.backButtonText}>Submit Form</Text>
+    </TouchableOpacity>
+  );
+
+  const isAllFilled = () =>
+    productName &&
+    categoryName &&
+    sku &&
+    description &&
+    weight &&
+    width &&
+    length &&
+    height &&
+    image &&
+    price;
+
+  const onPressSubmitFormButton = () => {
+    //TODO: I think categoryId and id in general should be better handled in the BE in API Contract for now it will be random
+    //In this case I will mkae categoryId as random, however the id itself is "Omit" from the ProductInterface as it's body JSON
+    const categoryId = new Date().valueOf();
+    const newProduct = {
+      CategoryId: categoryId,
+      categoryName: categoryName,
+      sku: sku,
+      name: productName,
+      description: description,
+      weight: parseInt(weight, 10),
+      width: parseInt(width, 10),
+      length: parseInt(length, 10),
+      height: parseInt(height, 10),
+      image: 'https://cf.shopee.co.id/file/7cb930d1bd183a435f4fb3e5cc4a896b',
+      harga: parseInt(price, 10),
+    };
+
+    addProduct(newProduct);
+  };
+
   return (
     <Container>
-      <Header title="Add Product" RenderAccessoryLeft={BackButton} />
-      <View style={{backgroundColor: 'red', flex: 1}} />
+      <Header
+        title="Add Product"
+        RenderAccessoryLeft={BackButton}
+        RenderAccessoryRight={SubmitFormButton}
+      />
+      <BoxSpace.C />
+      <View style={styles.mainContainer}>
+        <Text style={styles.headlineText}>
+          Add new item by filling the form below
+        </Text>
+        <BoxSpace.B />
+        <TextInput
+          placeholder="Name"
+          value={productName}
+          onChangeText={setProductName}
+          style={styles.input}
+        />
+        <BoxSpace.A />
+        <TextInput
+          placeholder="Category"
+          value={categoryName}
+          onChangeText={setCategoryName}
+          style={styles.input}
+        />
+        <BoxSpace.A />
+        <Wrapper>
+          <TextInput
+            placeholder="SKU"
+            value={sku}
+            onChangeText={setSku}
+            style={[styles.input, {flex: 1}]}
+          />
+          <BoxSpace.A />
+          <TextInput
+            placeholder="Price"
+            keyboardType="decimal-pad"
+            value={price}
+            onChangeText={setPrice}
+            style={[styles.input, {flex: 1}]}
+          />
+        </Wrapper>
+        <BoxSpace.A />
+        <TextInput
+          placeholder="Description"
+          value={description}
+          onChangeText={setDescription}
+          style={styles.largeInput}
+          multiline={true}
+        />
+        <BoxSpace.B />
+        <Text style={styles.headlineText}>Item size</Text>
+        <BoxSpace.A />
+        <Wrapper>
+          <TextInput
+            placeholder="Weight"
+            keyboardType="decimal-pad"
+            value={weight}
+            onChangeText={setWeight}
+            style={[styles.input, {flex: 1}]}
+          />
+          <BoxSpace.A />
+          <TextInput
+            placeholder="Width"
+            keyboardType="decimal-pad"
+            value={width}
+            onChangeText={setWidth}
+            style={[styles.input, {flex: 1}]}
+          />
+        </Wrapper>
+        <BoxSpace.A />
+        <Wrapper>
+          <TextInput
+            placeholder="Length"
+            keyboardType="decimal-pad"
+            value={length}
+            onChangeText={setLength}
+            style={[styles.input, {flex: 1}]}
+          />
+          <BoxSpace.A />
+          <TextInput
+            placeholder="Height"
+            keyboardType="decimal-pad"
+            value={height}
+            onChangeText={setHeight}
+            style={[styles.input, {flex: 1}]}
+          />
+        </Wrapper>
+        <BoxSpace.C />
+        <TextInput
+          placeholder="Image link"
+          value={image}
+          onChangeText={setImage}
+          style={styles.input}
+        />
+      </View>
     </Container>
   );
 };
